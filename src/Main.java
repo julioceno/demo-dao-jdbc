7fff1e2d7c9ea7e7408fd8cdc39bcde7e5948770
Julio <julio.nepomuceno@eurekalabs.com.br>
2024-04-20 18:39:38 -0300

    [feature]: seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bobgmail.com", new Date(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
