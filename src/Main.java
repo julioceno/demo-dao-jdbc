76d73f3ec523dd31b2a08711323f418103a5ce8e
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-04 22:39:26 -0300

    [feature]: FindBy id implementarion

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "books");
-
-        Seller seller = new Seller(21, "Bob", "bobgmail.com", new Date(), 3000.00, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
